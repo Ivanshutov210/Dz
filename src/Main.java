4b7374a9b837c18bb59bd9d114f4cef182b95361
Иван <ivan.shutov95@mail.ru>
2025-07-20 09:53:38 +0300

    WH 2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,21 @@ public class Main {
         }else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2014;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
     }
 }
